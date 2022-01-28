e9fdda1caa76767908ed4334cdec0dd0e3980ec4
homfarnam <homfarnam@gmail.com>
2022-01-28 22:50:13 +0330

    Remove white space

--- src/Wrapper/Layout.tsx
+++ src/Wrapper/Layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FunctionComponent<ILayoutProps> = ({ children, title }) => {
   return (
     <>
       <head>
-        <title>{title}</title>{' '}
+        <title>{title}</title>
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
       </head>
       <div className='flex flex-col justify-start w-full h-full min-h-screen'>
